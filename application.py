dc15d6496ecd4becd0bab24a6cabbf0bc6c8e035
austinntran <austin4tran@gmail.com>
2020-06-21 16:28:10 -0400

    Search results 0 error created

--- application.py
+++ application.py
@@ -109,7 +109,8 @@ def search():
         # if res.status_code != 200:
         #     raise Exception("ERROR: API request unsuccessful.")
         # return res.content# render_template("search.html", loggedIn=True)
-
+        if rows == 0:
+            return render_template("search.html", loggedIn=True, type="error", message="No results were found")
         return render_template("search.html", loggedIn=True, books=rows, search=post)
 
 
