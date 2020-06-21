4a886adad3790e30f923a3f1a40b1ad127e61138
austinntran <austin4tran@gmail.com>
2020-06-20 23:23:45 -0400

    Setup review submission and display

--- application.py
+++ application.py
@@ -76,7 +76,7 @@ def register():
         user = request.form.get("user")
         password = request.form.get("password")
         if not(re.match("^[a-zA-Z0-9_.-]+$", user) and re.match(r'[A-Za-z0-9@#$%^&+=\!]{8,}', password)):
-            return render_template("register.html", type="error", message="Username or password is invalid")
+            return render_template("register.html", type="error", message="Username or password is invalid or taken. Both should have no spaces, and passwords should be over 8 characters long.")
         user_exist = db.execute("SELECT * FROM users WHERE username = :user", {"user": user}).fetchone()
         if user_exist is None:
             password_hash = generate_password_hash(password)
@@ -84,7 +84,7 @@ def register():
             db.commit()
             return render_template("login.html", type="success", message="Account was successfully created!")
         else:
-            return render_template("register.html", type="error", message="Username or password you have entered is invalid or taken.")
+            return render_template("register.html", type="error", message="Username or password you have entered is invalid or taken. Both should have no spaces, and passwords should be over 8 characters long.")
 
 
 @app.route("/logout")
@@ -113,16 +113,27 @@ def search():
         return render_template("search.html", loggedIn=True, books=rows, search=post)
 
 
-@app.route("/book/<title>")
+@app.route("/book/<title>", methods=["GET", "POST"])
 def book(title):
     if session.get("user_id") is None:
         return redirect("/login")
+
     book = db.execute("SELECT * FROM books WHERE UPPER(title) = :title", {"title": title.upper()}).fetchone()
     # use goodreads API
     if book is None:
         return redirect("/search")
     res = requests.get("https://www.goodreads.com/book/review_counts.json", params={"key": "Z9EerpBcRErsobbjgfc9g", "isbns": book.isbn})
-    res2 = requests.get("https://www.goodreads.com/search/index.xml", params={"key": "Z9EerpBcRErsobbjgfc9g", "q": book.isbn, "search[field]": "all"})
     data = res.json()
-    data2 = res2.content
-    return render_template("book.html", loggedIn=True, title=title, data=data["books"][0], book=book)
+    reviews = db.execute("SELECT username, text, rating FROM reviews JOIN users ON reviews.user_id = users.id WHERE book_id = :isbn", {"isbn": book.isbn}).fetchall()
+    if request.method == "GET":
+        #db.execute("SELECT * FROM users WHERE username = :user", {"user": user}).fetchone()
+        return  render_template("book.html", loggedIn=True, data=data["books"][0], book=book, reviews=reviews)
+    elif request.method == "POST":
+        preexist = db.execute("SELECT * FROM reviews WHERE user_id = :user_id and book_id = :isbn", {"user_id": session["user_id"], "isbn": book.isbn})
+        if preexist:
+            return render_template("book.html", loggedIn=True, data=data["books"][0], book=book, type="error", message="You have already created review for this book", reviews=reviews)
+        text = request.form.get("text")
+        rating = request.form.get("rating")
+        db.execute("INSERT INTO reviews (user_id, book_id, text, rating) VALUES (:user_id, :book_id, :text, :rating)", {"user_id": session["user_id"], "book_id": book.isbn, "text": text, "rating": rating})
+        db.commit()
+        return  render_template("book.html", loggedIn=True, data=data["books"][0], book=book, type="success", message="Review sucessfully posted", reviews=reviews)
